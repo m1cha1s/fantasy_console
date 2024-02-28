28dc8461f77fae9c75a01c1003227458b4b8aec1
m1cha1s <msulej04@gmail.com>
2024-02-28 23:21:36 +0100

    There may be an issue with raylib that causes the SegFault

--- examples/hello.lua
+++ examples/hello.lua
@@ -5,13 +5,38 @@ function Init()
     print("INIT")
 end
 
+local pos = {
+    x = 0,
+    y = 0,
+}
+
+local vel = {
+    x = 100,
+    y = 10,
+}
+
 function Update(dt)
     --print("dt: " .. dt)
+    pos.x = pos.x + vel.x*dt
+    pos.y = pos.y + vel.y*dt
+
+    if pos.x > 800 then
+        vel.x = - vel.x
+    end
+    if pos.y > 800 then
+        vel.y = - vel.y
+    end
+    if pos.x < 0 then
+        vel.x = - vel.x
+    end
+    if pos.y < 0 then
+        vel.y = - vel.y
+    end
 end
 
 function Draw()
     gfx.clear(BACKGROUND)
-    gfx.text("Balls", 200, 200, 40, BLACK)
+    gfx.text("Balls", pos.x, pos.y, 40, BLACK)
 end
 
 function Deinit()
