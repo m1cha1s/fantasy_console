33322ac9490e621751d192f35e378eee793eb559
m1cha1s <msulej04@gmail.com>
2024-03-05 15:01:25 +0100

    Added controls and more physics to platformer example

--- examples/platformer.lua
+++ examples/platformer.lua
@@ -13,7 +13,11 @@ local player = {
     },
     w = 20,
     h = 40,
+
+    onGround = false,
 }
+
+
 
 function player:update(dt)
     self.vel.x = self.vel.x + self.acc.x*dt
@@ -26,17 +30,56 @@ function player:update(dt)
     self.acc.y = 0
 end
 
+local function applyGravity()
+    player.acc.y = player.acc.y + 98
+end
+
+local function applyFriction(coeff)
+    player.acc.x = player.acc.x - player.vel.x*coeff
+    player.acc.y = player.acc.y - player.vel.y*coeff
+end
+
 function Init()
 
 end
 
 function Update(dt)
+    applyGravity()
     player:update(dt)
+
+    -- Simple ground collision
+    if player.pos.y > 600 then
+        player.pos.y = 600
+        player.vel.y = 0
+
+        player.onGround = true
+        applyFriction(3)
+    else
+        player.onGround = false
+    end
+
+    -- Handle input
+    if btn.left() and player.onGround then
+        player.acc.x = player.acc.x - 300
+    end
+    if btn.right() and player.onGround then
+        player.acc.x = player.acc.x + 300
+    end
+    if btn.up() and player.onGround then
+        player.acc.y = player.acc.y - 10000
+    end
 end
 
 function Draw()
     gfx.clear({r=255,g=255,b=255})
-    gfx.rect(player.pos.x+player.w/2, player.pos.y + player.h, player.w, player.h, {r=255})
+    gfx.rect(player.pos.x-player.w/2, player.pos.y - player.h, player.w, player.h, {r=255})
+
+    -- Draw ground
+    gfx.rect(
+        0, 600,
+        800, 200,
+        {}
+    )
 end
 
 function Deinit()
